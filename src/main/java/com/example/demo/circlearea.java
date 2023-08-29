6121f78c44b25b7816b35c8dd6cfca2d1a88fd4c
onkarkadere11 <onkarkadere11@gmail.com>
2023-08-29 07:07:33 +0530

    abc

---
+++ src/main/java/com/example/demo/circlearea.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class circlearea {
+	@RequestMapping("/sayHi")
+	public String sayHi(@RequestParam String str1, @RequestParam String str2) {
+		return " hii, "+ str1 +" "+ str2;
+		
+	}
+	@RequestMapping("/area")
+	public String area(@RequestParam float r) {
+		return "area of circle having radius" + r + "is "+ " "+(3.14*r*r);
+	}
+
+}
